2abb29bf1a2c7736d7bb0b7bf46723259ba902bd
YoannDEWILDE <yoann.dewilde@etudiant.univ-rennes1.fr>
2023-02-10 11:20:42 +0100

    Partie 4: DnD

--- interaction.js
+++ interaction.js
@@ -2,11 +2,50 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  console.log("Construit DnD");
+  // Définir ici les attributs de la 'classe'
+  this.xIni = 0;
+  this.yIni = 0;
+  this.xFin = 0;
+  this.yFin = 0;
 
-	// Developper les 3 fonctions gérant les événements
+  this.mousePressed = false;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+  // Developper les 3 fonctions gérant les événements
+  this.dragStart = function (evt) {
+    let pos = getMousePosition(canvas, evt);
+    this.xIni = pos.x;
+    this.yIni = pos.y;
+
+    this.mousePressed = true;
+    console.log("Start DnD: ", pos);
+  }.bind(this);
+
+
+  this.drag = function (evt) {
+    if (this.mousePressed) {
+      let pos = getMousePosition(canvas, evt);
+      this.xEnd = pos.x;
+      this.yEnd = pos.y;
+      console.log("During DnD: ", pos);
+    }
+  }.bind(this);
+
+  this.dragEnd = function (evt) {
+    let pos = getMousePosition(canvas, evt);
+    this.xEnd = pos.x;
+    this.yEnd = pos.y;
+
+    this.mousePressed = false;
+    console.log("End DnD: ", pos);
+  }.bind(this);
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener("mousedown", this.dragStart);
+  canvas.addEventListener("mousemove", this.drag);
+  canvas.addEventListener("mouseup", this.dragEnd);
+
+  console.log(this, canvas);
 };
 
 
